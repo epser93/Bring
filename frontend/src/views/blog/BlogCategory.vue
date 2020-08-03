1f5cd1c94d21d29f3128ce1cb2e73d1ffb2c24d2
한정연 <51sarang@hanmail.net>
2020-08-03 13:12:52 +0900

    0803 edit blog category-2

--- frontend/src/views/blog/BlogCategory.vue
+++ frontend/src/views/blog/BlogCategory.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="blogCategory">
     <h2>카테고리</h2>
+
+    <h3 class="text-left">카테고리 수정</h3>
     <!-- 기존 카테고리 -->
-    <div class="row col-6">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item" v-for="category in categoryList" v-bind:key="category.name">{{ category.name }}</li>
-      </ul>
+    <div class="row form-group">
+      <div class="col-2 text-left"><label for="multiple-select" class=" form-control-label">카테고리 선택: </label></div>
+      <div class="col-10 text-left">
+        <select v-model="boardName">
+          <option v-for="category in categoryList" v-bind:key="category.name">
+            {{ category.name }}
+          </option>
+        </select>
+      </div>
     </div>
+
+    <!-- 카테고리 수정 -->
+    <form class="form-inline mb-5">
+        <label>카테고리 수정:</label>
+        <input class="form-control mr-sm-2 ml-5" v-model="editCategoryName" placeholder="새 카테고리 이름" aria-label="editCategory">
+        <button class="btn btn-outline-secondary my-2 my-sm-0" @click="putCategory" type="submit">수정</button>
+    </form>
 
     <!-- 카테고리 생성 -->
     <form class="form-inline mb-5">
@@ -32,6 +46,8 @@ export default {
       newCategoryName: '',
       nickname: this.$route.params.nickname,
       categoryList: [],
+      boardName:'',
+      editCategoryName: '',
     }
   },  
 
@@ -52,15 +68,34 @@ export default {
             console.error(err)
           })
     },
-    getCategory() {
-          axios.get(`${BACK_URL}/blog/${this.nickname}/categories`)
-              .then(res => {
-                  this.categoryList = res.data.list
-              })
+    putCategory() {
+      const config = {
+          headers: {
+            'X-AUTH-TOKEN' : this.$cookies.get('X-AUTH-TOKEN')
+          }
+        }
+        axios.put(
+          `${BACK_URL}/blog/${this.nickname}/${this.boardName}`, {
+            params: {
+              name: this.editCategoryName,
+            }}, config)
 
-              .catch(err => {
-                  console.log(err)
-              })
+          .then(() =>{
+          })
+          .catch((err) => {
+            alert('실패')
+            console.error(err)
+          })
+    },
+    getCategory() {
+      axios.get(`${BACK_URL}/blog/${this.nickname}/categories`)
+          .then(res => {
+              this.categoryList = res.data.list
+          })
+
+          .catch(err => {
+              console.log(err)
+          })
     },
   },
   created() {
