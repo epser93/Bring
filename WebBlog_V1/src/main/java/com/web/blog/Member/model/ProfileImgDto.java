9e51e188d225870bced192e431ba06e97ebf644d
lawliet0521 <lawliet0521@gmail.com>
2020-08-08 00:42:26 +0900

    0808_1_commit: Profile image upload method added.

---
+++ WebBlog_V1/src/main/java/com/web/blog/Member/model/ProfileImgDto.java
@@ -0,0 +1,32 @@
+package com.web.blog.Member.model;
+
+import com.web.blog.Member.entity.ProfileImg;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ProfileImgDto {
+    private Long id;
+    private String filePath;
+    private String imgFullPath;
+    private Long msrl;
+
+    public ProfileImg toEntity(){
+        ProfileImg build = ProfileImg.builder()
+                .id(id)
+                .filePath(filePath)
+                .msrl(msrl)
+                .build();
+        return build;
+    }
+
+    @Builder
+    public ProfileImgDto(Long id, String filePath, String imgFullPath, Long msrl) {
+        this.id = id;
+        this.filePath = filePath;
+        this.imgFullPath = imgFullPath;
+        this.msrl = msrl;
+    }
+}
