56f53ebb5c33c7ab11b649c8f8cb4e5141fc84db
Eunseok <eunsuk93@naver.com>
2020-08-11 17:24:50 +0900

    add tag

--- frontend/src/views/post/UpdateForm.vue
+++ frontend/src/views/post/UpdateForm.vue
@@ -36,6 +36,12 @@
     <!-- 글 에디터 부분 -->
     <v-md-editor class="text-left" v-model="postData.content" height="600px"></v-md-editor>
     <br>
+
+    <!-- 태그 -->
+    <div class="tag">
+      <span v-for="(tag,index) in postData.tags" :key="index" class="badge badge-pill badge-light mr-2 p-2" @click="deleteTag(index)">{{ tag }}</span>
+    </div>
+    <input placeholder="태그를 입력해주세요" class="mb-5 tag-input" type="text" v-model="tag" @keydown.enter="postTag">
     
   </div>
 </template>
@@ -62,7 +68,7 @@ export default {
       postData: {
         subject: '',
         content: '',
-        tags: [''],
+        tags: [],
       },
 
       member_nickname: null,
@@ -70,6 +76,8 @@ export default {
       categoryList: [],
 
       thumbnail: '',
+      tag:""
+     
     }
   },
   methods: {
@@ -82,6 +90,7 @@ export default {
       axios.put(`${BACK_URL}/blog/${this.nickname}/${this.boardName}/${this.post_id}`, this.postData, config)
         .then(() => {
           this.thumbnailPost()
+          console.log('포스트데이터',this.postData)
         })
         .catch(err => console.log(err))
     },
@@ -94,42 +103,57 @@ export default {
                   console.log(err)
               })
       },
-      getPostInfo() {
-        axios.get(`${BACK_URL}/blog/${this.nickname}/${this.boardName}/${this.post_id}`)
-          .then(res => {
-            this.member_nickname = res.data.list[0].list[0].member_nickname
-            this.postData.subject = res.data.list[0].list[0].subject
-            this.postData.content = res.data.list[0].list[0].content
-            this.createdAt = res.data.list[0].list[0].createdAt
-          })
-          .catch(err => console.log(err))
-      },
+    getPostInfo() {
+      axios.get(`${BACK_URL}/blog/${this.nickname}/${this.boardName}/${this.post_id}`)
+        .then(res => {
+          this.member_nickname = res.data.list[0].list[0].member_nickname
+          this.postData.subject = res.data.list[0].list[0].subject
+          this.postData.content = res.data.list[0].list[0].content
+          this.createdAt = res.data.list[0].list[0].createdAt
+          this.postData.tags = res.data.list[1].list
+        })
+        .catch(err => console.log(err))
+    },
 
-      // 썸네일 관련
-      thumbnailSelect() {
-        this.thumbnail = this.$refs.thumbnailImage.files[0]
-      },
-      thumbnailPost() {
-        const formData = new FormData()
-        formData.append('files', this.thumbnail)
-
-        const config = {
-            headers: {
-              'X-AUTH-TOKEN' : this.$cookies.get('X-AUTH-TOKEN'),
-              'Content-Type' : 'multipart/form-data'
-            }
-          }
-          axios.post(
-            `${BACK_URL}/blog/${this.nickname}/${this.boardName}/${this.post_id}/uploads`, formData, config)
-
-            .then(() =>{
-              this.$router.go(-1)
-            })
-            .catch((err) => {
-              alert('카테고리를 선택해 주세요')
-              console.error(err)
-            })      
+    postTag() {
+      if (!this.postData.tags.includes(this.tag)) {
+        this.postData.tags.push(this.tag)
+        this.tag = ""
+      } else {
+        alert('중복된 태그입니다.')
+        this.tag= ""
       }
+      console.log('태그추가', this.postData.tags)
+    },
+    deleteTag(index) {
+      this.postData.tags.splice(index,1)
+    },
+
+    // 썸네일 관련
+    thumbnailSelect() {
+      this.thumbnail = this.$refs.thumbnailImage.files[0]
+    },
+    thumbnailPost() {
+      const formData = new FormData()
+      formData.append('files', this.thumbnail)
+
+      const config = {
+          headers: {
+            'X-AUTH-TOKEN' : this.$cookies.get('X-AUTH-TOKEN'),
+            'Content-Type' : 'multipart/form-data'
+          }
+        }
+        axios.post(
+          `${BACK_URL}/blog/${this.nickname}/${this.boardName}/${this.post_id}/uploads`, formData, config)
+
+          .then(() =>{
+            this.$router.go(-1)
+          })
+          .catch((err) => {
+            alert('카테고리를 선택해 주세요')
+            console.error(err)
+          })      
+    }
   },
   created() {
     this.getPostInfo()
@@ -137,6 +161,15 @@ export default {
   }
 };
 </script>
+<style scoped>
+.tag-input {
+  width: 100%
+}
+
+.badge {
+  cursor: pointer;
+}
+</style>
 
 
 
