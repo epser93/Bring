1218bd0d246bce7990914aac80bf11b89e292c9d
Eunseok <eunsuk93@naver.com>
2020-08-13 17:15:11 +0900

    fix main page

---
+++ frontend/src/views/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    index page
+    <router-link :to="{ name : 'RecentlyPost' }">시작하기</router-link>
+    <!-- <a href="http://localhost:8081/postqna/recentlypost">시작하기2</a> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  created () {
+    this.$cookies.set('mode', 'Blog')
+  }
+
+}
+</script>
+
+<style>
+
+</style>
