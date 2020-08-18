846bc6b0232821ecf802f1d5176920c382cf2bb1
한정연 <51sarang@hanmail.net>
2020-08-18 18:46:36 +0900

    0818 edit header

--- frontend/src/components/common/Header.vue
+++ frontend/src/components/common/Header.vue
@@ -1,27 +1,33 @@
 <template>
     <div id="header">
-        <nav class="navbar navbar-expand navbar-light bg-light">
-            <router-link v-if="this.mode === 'Blog'" :to="{ name: 'RecentlyPost' }" class="navbar-brand">
+        <nav class="navbar navbar-expand-lg p-0 m-0">
+            <router-link v-if="this.mode === 'Blog'" :to="{ name: 'RecentlyPost' }" class="navbar-brand p-3">
                 GEESHIQUEEN블로그
             </router-link>
-            <router-link v-if="this.mode === 'QnA'" :to="{ name: 'RecentlyQuestion' }" class="navbar-brand">
+            <router-link v-if="this.mode === 'QnA'" :to="{ name: 'RecentlyQuestion' }" class="navbar-brand p-3">
                 GEESHIQUEEN지식인
             </router-link>
-
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"><i class="fas fa-sliders-h"></i></span>
+            </button>
+            <div class="collapse navbar-collapse m-0" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto text-left">
                     <li class="nav-item">
                         <a href='#' v-if="!isLogin" @click="$modal.show('demo-login')" class="nav-link">LOGIN</a>
                     </li>
+                    <span class="vertical-line mx-3"></span>
                     <li class="nav-item">
                         <router-link v-if="isLogin" :to="{ name: 'Profile', query: { nickname: this.nickname }}" class="nav-link">프로필</router-link> 
                     </li>
+                    <span class="vertical-line mx-3 my-2"></span>
                     <li class="nav-item">
                         <router-link v-if="isLogin" :to="{ name: 'MyBlog', params: { nickname: this.nickname }}" class="nav-link">블로그</router-link> 
                     </li>
+                    <span class="vertical-line mx-3 my-2"></span>
                     <li class="nav-item">
                         <router-link v-if="isLogin" :to="{ name: 'Question' }" class="nav-link">지식인</router-link> 
-                    </li>                    
+                    </li>     
+                    <span class="vertical-line mx-3 my-2"></span>               
                     <li class="nav-item" id="logout">
                         <p v-if="isLogin" @click="logout" class="nav-link">로그아웃</p> 
                     </li>
@@ -29,29 +35,30 @@
 
                 <!-- 검색창 -->
                 <div v-if="this.mode === 'Blog'" class="form-inline my-2 my-lg-0">
-                    <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle btn btn-outline-success">{{ keywordType.name }}</button>
+                    <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle btn btn-outline-secondary">{{ keywordType.name }}</button>
                     <div tabindex="-1" aria-hidden="true" role="menu" class="dropdown-menu">
                         <button type="button" tabindex="0" @click="dropdown(typeid, value)" class="dropdown-item" v-for="(value, typeid) in dropdownList" v-bind:key="typeid">
                             {{ value }}
                         </button>
                     </div>
                     <input class="form-control" type="search" v-model="keyword" placeholder="키워드 입력" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" @click="gotoSearch">검색</button>
+                    <a class="mx-3" @click="gotoSearch"><i class="fas fa-search"></i></a>
                 </div>
 
                 <!-- 질문 검색창 -->
-                <div v-if="this.mode === 'QnA'" class="form-inline my-2 my-lg-0">
-                    <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle btn btn-outline-success">{{ keywordType.name }}</button>
+                <div v-if="this.mode === 'QnA'" class="form-inline">
+                    <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle btn btn-outline-secondary">{{ keywordType.name }}</button>
                     <div tabindex="-1" aria-hidden="true" role="menu" class="dropdown-menu">
                         <button type="button" tabindex="0" @click="dropdown(typeid, value)" class="dropdown-item" v-for="(value, typeid) in dropdownList" v-bind:key="typeid">
                             {{ value }}
                         </button>
                     </div>
                     <input class="form-control" type="search" v-model="keyword" placeholder="키워드 입력" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" @click="gotoSearchQuestions">검색</button>
+                    <a class="mx-3" @click="gotoSearchQuestions"><i class="fas fa-search"></i></a>
                 </div>                
             </div>
         </nav>
+        <hr class="m-0">
         
     </div>
 </template>   
@@ -151,5 +158,50 @@
 <style scoped>
 #logout {
   cursor: pointer;
+}
+
+.navbar a:visited{
+    color: #494949;
+}
+
+.navbar li {
+  color:  #494949;
+}
+
+.navbar p {
+  color: #494949;
+}
+
+input {
+    width:210px;
+    height: auto; /* 높이값 초기화 */ 
+    line-height : normal; /* line-height 초기화 */ 
+    padding: .7em .5em; /* 원하는 여백 설정, 상하단 여백으로 높이를 조절 */ 
+    font-family: inherit; /* 폰트 상속 */ 
+    border: 1px solid #999; 
+    border-radius: 0; /* iSO 둥근모서리 제거 */ 
+    outline-style: none; /* 포커스시 발생하는 효과 제거를 원한다면 */ 
+    -webkit-appearance: none; /* 브라우저별 기본 스타일링 제거 */ 
+    -moz-appearance: none; appearance: none;
+}
+
+input:hover {
+  border: 1px solid #56dbc9;
+  text-decoration: none;
+  transition-duration: 0.3s;
+}
+
+button {
+    height: auto; /* 높이값 초기화 */ 
+    line-height : normal; /* line-height 초기화 */ 
+    padding: .7em .5em; /* 원하는 여백 설정, 상하단 여백으로 높이를 조절 */ 
+    font-family: inherit; /* 폰트 상속 */ 
+    border-radius: 0; /* iSO 둥근모서리 제거 */ 
+}
+
+.navbar a:hover {
+  color: #56dbc9;
+  text-decoration: none;
+  transition-duration: 0.3s;
 }
 </style>
