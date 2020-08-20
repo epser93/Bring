992c44e54f6f7eed98acba55eb6a57286293f490
Eunseok <eunsuk93@naver.com>
2020-08-20 14:31:12 +0900

    fix search layout

--- frontend/src/components/common/SearchQuestions.vue
+++ frontend/src/components/common/SearchQuestions.vue
@@ -1,25 +1,37 @@
 <template>
   <div>
+    <h1 id="search-heading"> {{ this.keyword }} 검색결과</h1>
     <!-- 글 리스트 -->
     <div class="col-10 container">
         <div class="text-left ml-5 mt-5" v-if="postList.length == 0">
             <h3>해당 내용의 글이 없습니다</h3>
         </div>
-        <div class="row">
-            <div v-for="(item, index) in postList" :key="item.postId" class="p-0 mb-5 col-12 col-lg-3">
-                <div class="card" style="width: 75%;">
-                    <img class="card-img-top" :src="thumbnail1[index]" alt="Card image cap" style="height: 150px;">
-                    <div class="card-body pb-0">
-                        <h5 class="card-title">{{ item.subject.slice(0, 10) + '...'  }}</h5>
-                        <p class="card-text mb-3">{{ item.content.slice(0, 20) + '...' }}</p>
+        <section class="cards row">
+            <div class="col-lg-12 row">
+                <div v-for="(post, index) in postList" :key="post.postId" class="card1 col-lg-3 col-md-4 col-sm-6 col-12">
+                <div class="cardwrap">
+                    <div v-if="post.selectOver" id="solved">solved!</div>
+                    <div class="card-body p-0" @click="gotoDetail(post)">
+                    <div class="img-section" :style="{ 'background-image' : `url(${thumbnails[index]})`}">
+                        <a href=""></a>
                     </div>
-                    <div class="card-footer bg-transparent">
-                        <button class="btn btn-sm" @click="gotoDetail(item)">글 보기</button>
+                    <div class="contents">
+                        <h4>{{ post.subject }}</h4>
+                        <!-- <p>{{ post.content }}</p> -->
+                        <!-- <v-md-preview :text="post.content"></v-md-preview> -->
+                        <p class="comment-date">{{ post.createdAt.substring(0,10) }} · {{ post.replyCnt }}개의 댓글</p>
+                    </div>
+                    </div>
+                    <div class="writer-info">
+                    <button class="btn btn-sm" @click="gotoUserInfo(post.member_nickname)">{{ post.member_nickname }}</button>
+                    <p>♥ {{ post.likes }}</p>
                     </div>
                 </div>
+                </div>
             </div>
-        </div>
+        </section>    
     </div>
+    <infinite-loading @infinite="infiniteHandler"></infinite-loading>
   </div>
 </template>
 
@@ -34,48 +46,52 @@ export default {
             keyword: this.$route.query.q,
             type: this.$route.query.type,
             postList: [],
-            thumbnail1: [],
-            userNow: this.$cookies.get('nickname'), 
+            thumbnails: [],
+            page : 1
         }
     },
     methods: {
-        // 검색
-        searchFor() {
+        infiniteHandler($state) {
             const config = {
                 headers: {
                     'X-AUTH-TOKEN' : this.$cookies.get('X-AUTH-TOKEN'),
                 }
             }
-            axios.get(`${BACK_URL}/questions/search/all_questions/${this.keyword}/${this.type}`, config)
+            axios.get(`${BACK_URL}/questions/search/all_questions/${this.keyword}/${this.type}?no=${this.page}`, config)
                 .then(res => {
-                    this.postList = res.data.list[0].list
-                    this.thumbnail1 = res.data.list[1].list
+                    if (res.data.list[0].list.length) {
+                        this.page += 1
+                        this.postList.push(...res.data.list[0].list)
+                        console.log(this.postList)
+                        this.thumbnails.push(...res.data.list[1].list)
+                        $state.loaded()
+                    } else {
+                        $state.complete()
+                    }
                 })
-
-                .catch(err => {
-                    console.log(err)
-                })
+                .catch (err => console.log(err))
         },
-
         // 포스트 디테일
         gotoDetail(post) {
             this.$router.push({ name : "QuestionDetail" , params: { nickname : post.member_nickname, qpostId : post.qpostId }})
         },   
+        gotoUserInfo(userNickname) {
+            this.$router.push({ name : "Profile" , query: { nickname : userNickname }})
+        },  
     },
     mounted() {
-        this.searchFor()
     },
     watch: {
         '$route.query.q'() {
             this.keyword = this.$route.query.q
             this.type = this.$route.query.type
-            this.searchFor()
+            location.reload()
         },
 
         '$route.query.type'() {
             this.keyword = this.$route.query.q
             this.type = this.$route.query.type
-            this.searchFor()
+            location.reload()
         }
     }
 }
