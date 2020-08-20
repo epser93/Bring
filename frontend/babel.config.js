664b1bbfd12d0e062cab381606d4cdd68ec06cff
lawliet0521 <lawliet0521@gmail.com>
2020-08-21 04:15:12 +0900

    0821_10_commit

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  configureWebpack: {
+    // other webpack options to merge in ...
+  },
+  // devServer Options don't belong into `configureWebpack`
+  devServer: { host: "0.0.0.0", hot: true, disableHostCheck: true },
 }
